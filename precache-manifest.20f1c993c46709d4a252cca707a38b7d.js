1d4eeb6dad0cbbdcf786fa32382845b6d65c1bf0
Salifov <salifov17@gmail.com>
2020-07-29 14:36:43 +0300

    Updates

---
+++ precache-manifest.20f1c993c46709d4a252cca707a38b7d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "590e8cb6709ce4967339ec54da017540",
+    "url": "/react-beach-resort/index.html"
+  },
+  {
+    "revision": "01a940eaf79d92a815dd",
+    "url": "/react-beach-resort/static/css/main.c72fa122.chunk.css"
+  },
+  {
+    "revision": "1c1f2daec7ad1bb1f2be",
+    "url": "/react-beach-resort/static/js/2.ef0e2ab1.chunk.js"
+  },
+  {
+    "revision": "0c559e1ae0a9426ead1485b442d61f0b",
+    "url": "/react-beach-resort/static/js/2.ef0e2ab1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "01a940eaf79d92a815dd",
+    "url": "/react-beach-resort/static/js/main.71c5f835.chunk.js"
+  },
+  {
+    "revision": "0388845f09a4a663c824",
+    "url": "/react-beach-resort/static/js/runtime-main.7e1d7184.js"
+  },
+  {
+    "revision": "aebaec51b2f82a2993fda821ca04e228",
+    "url": "/react-beach-resort/static/media/defaultBcg.aebaec51.jpeg"
+  },
+  {
+    "revision": "e9ea73949de71d45ae46047646b95727",
+    "url": "/react-beach-resort/static/media/loading-arrow.e9ea7394.gif"
+  },
+  {
+    "revision": "9f0cccc1e406a5aa9ef6ee36229762c3",
+    "url": "/react-beach-resort/static/media/logo.9f0cccc1.svg"
+  },
+  {
+    "revision": "e928a5c462a9f4f590af64285b387571",
+    "url": "/react-beach-resort/static/media/room-1.e928a5c4.jpeg"
+  },
+  {
+    "revision": "052c76815f889f20b8a498c19a212c50",
+    "url": "/react-beach-resort/static/media/room-2.052c7681.jpeg"
+  }
+]);
